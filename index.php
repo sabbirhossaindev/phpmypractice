57dd63a6a9d757b0908d8dd299e4723a649a918b
sabbir hossain <mdsabbir477470@gmail.com>
2023-08-05 12:30:29 +0600

    string done

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+echo "হ্যালো বাংলাদেশ!";
+echo "হ্যালো পিএইচপি!<br>";
+// একাধিক প্যারামিটার ব্যবহার করে
+echo "স্যাট একাডেমী " , "বাংলাদেশের " , "বৃহত্তম " , "অনলাইন/অফলাইন " , "ফ্রী " , "আইটি " , "একাডেমী। <br>";
+
+$textPartOne = "আমরা সবাই ";
+$textPartTwo = "আইটি প্রেমী";
+
+print "$textPartOne $textPartTwo <br>";
+print "আমরা সবাই $textPartTwo <br>";
+
+define("number", "01888559287");
+echo number;
+
+
+
+
+?>
